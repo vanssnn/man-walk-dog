039aefe15d9d69d0460ef6a5f0b290d39fe62dcf
Jessica Ryan <jessicaryan0419@gmail.com>
2026-02-07 20:31:31 +0700

    feat: create menu

--- rooms/menu.gd
+++ rooms/menu.gd
@@ -4,6 +4,9 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_pressed():
 		start_game()
 		
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		start_game()
 
 func start_game() -> void:
 	SceneManager.change_scene('res://rooms/level1.tscn', { "pattern": "squares" })
